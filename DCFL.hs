9e8eab71c71863062eaacd161ab08c2f0dab265c
Dhaivat Pandya <dhaivatpandya@gmail.com>
2015-07-05 00:45:06 +0530

    Added cabal

--- DCFL.hs
+++ DCFL.hs
@@ -15,13 +15,14 @@ data Variable = Variable {possible::[Int], valueIndex::Int,
   distr::Distribution} deriving (Show)
 --instance Show Variable where
 --  show (Variable possible valueIndex _) = show $ possible !! valueIndex
-
-data ConstraintEl = ConstraintEl {variableIndex1 :: Int, variableIndex2 :: Int,
+-- Variable indices describe 
+data ConstraintEl = ConstraintEl {variableIndices :: [Int],
   constraint :: ([Int] -> Bool)}
+data Solved = Solved {variables :: [Variable], iterationCount :: Int}
 
 instance Show ConstraintEl where
-  show (ConstraintEl variableIndex1 variableIndex2 _) = 
-    "Constraint " ++ (show variableIndex1) ++ " " ++ (show variableIndex2)
+  show (ConstraintEl variableIndices _) = 
+    "Constraint " ++ (show variableIndices)
 
 width (Distribution p) = fromIntegral $ length p
 
@@ -87,3 +88,79 @@ evalConstraints constraints values =
 applyAt f index list = 
   map (\x -> if (snd x) == index then f (fst x)
                                  else (fst x)) $ zip list [0..]
+
+-- get the constraints associated with a certain variable index
+getConstraintsFor :: Int -> [ConstraintEl] -> [[Int] -> Bool]
+getConstraintsFor n constraintSet = 
+  [constraint | ConstraintEl [a, b] constraint <- constraintSet, ((a == n) || (b == n))]
+
+-- unwrapping utility
+getConstraintFromEl (ConstraintEl _ constraint) = constraint
+
+-- get the constraint functions out of a list of ConstraintEls
+justConstraints = map getConstraintFromEl
+
+-- get values of the variable set
+getValues variables = map (\(Variable _ val _) -> val) variables
+
+-- randomizes the variable value of a single variable in a list of variables
+randomizeSingle::Int -> [Variable] -> [IO Variable]
+randomizeSingle variableIndex variables = 
+  map (\x -> if (snd x) == variableIndex then randomizeVariable $ fst x
+                                         else return $ (fst x)) $ zip variables [0..]
+
+-- randomize all the variables in a list
+randomize variables = map randomizeVariable variables
+
+printVariables variables = do
+  map (putStrLn . show) variables
+
+-- either randomize or let a variable stay, depending on what the constraint
+-- check tells us
+update :: Int -> [Variable] -> [ConstraintEl] -> IO [Variable]
+update variableIndex variables constraintSet = do
+  rvariables <- sequence $ randomizeSingle variableIndex variables
+  let values = getValues rvariables
+      constraints = getConstraintsFor variableIndex constraintSet 
+      constraintRes = evalConstraints constraints values
+
+      -- update the variable probability based on the value of constraintRes
+      appliedVars = applyAt (\var -> updateVariableProb var constraintRes) 
+        variableIndex rvariables in
+      return appliedVars
+
+-- update each variable in the indices list once
+updateEach' :: [Variable] -> [ConstraintEl] -> [Int] -> IO [Variable]
+updateEach' variables constraintSet (i:indices)
+  | length indices > 0 = do
+    vars <- update i variables constraintSet
+    updateEach' vars constraintSet indices
+  | otherwise  = do
+    return variables
+
+-- update each variable in the variable set based on the constraint set
+-- value
+updateEach :: [Variable] -> [ConstraintEl] -> IO [Variable]
+updateEach variables constraintSet = 
+  updateEach' variables constraintSet [0 .. (length variables)]
+
+-- update the variable set n number of times
+updateEachTimes :: [Variable] -> [ConstraintEl] -> Int -> IO [Variable]
+updateEachTimes variables constraintSet n
+  | n > 0 = do
+    rvars <- updateEach variables constraintSet
+    updateEachTimes rvars constraintSet (n - 1)
+  | otherwise = return variables
+
+-- checks if every probability in the distribution is either 0 or 1
+checkDistrSolved (Distribution probab) = all (\x -> x == 0.0 || x == 1.0) probab
+
+-- check if the constraints have been solved by looking at the distributions
+-- of each 
+checkSolved [] = True
+checkSolved (var:vars)
+  | checkDistrSolved $ distr var = checkSolved vars
+  | otherwise = False
+
+
+
--- DCFL_runner.hs
+++ DCFL_runner.hs
@@ -1,91 +1,55 @@
 import DCFL
+import Data.List
 
 -- creates the set of variables; n colors possible
 colorVariables n = 
   map (\x -> Variable [0..n-1] 0 (initDistribution n)) $ [0..n-1]
+
+-- create a set of m variables that can take on values between 0 and n-1
+-- inclusive.
+finiteVariables m n = 
+  map(\x -> Variable [0..n-1] 0 (initDistribution n)) $ [0..m-1]
 
 -- constraint function for two different nodes in the network
 -- specifies that their colors should not be equal
 colorConstraint :: Int -> Int -> [Int] -> Bool
 colorConstraint node1 node2 values = (values !! node1) /= (values !! node2)
 
--- this is a constraint that forces one of the nodes to have a color value of 4
-nonsenseConstraint node1 node2 values = (values !! node1) == 4
-
 -- construct the constraint set by currying
 colorConstraints :: Int -> [ConstraintEl]
 colorConstraints n = 
-  [ConstraintEl a b $ colorConstraint a b | a <- [0..n-1], b <- [0..n-1], a /= b]
+  [ConstraintEl [a, b] $ 
+  colorConstraint a b | a <- [0..n-1], b <- [0..n-1], a /= b]
 
--- get the constraints associated with a certain variable index
-getConstraintsFor :: Int -> [ConstraintEl] -> [[Int] -> Bool]
-getConstraintsFor n constraintSet = 
-  [constraint | ConstraintEl a b constraint <- constraintSet, ((a == n) || (b == n))]
+constraintOne values = (values !! 0) + (values !! 1) == 40
+constraintTwo values = 2*(values !! 0) + 3*(values !! 1) == 100
+constraints = [ConstraintEl [0, 1] constraintOne, 
+  ConstraintEl [0, 1] constraintTwo]
 
--- unwrapping utility
-getConstraintFromEl (ConstraintEl _ _ constraint) = constraint
+solve :: [Variable] -> [ConstraintEl] -> IO Solved
+solve vars constraints = do
+  rvars <- updateEachTimes vars constraints 10
+  if checkSolved rvars 
+    then return $ Solved rvars 0
+    else do 
+      solved <- solve rvars constraints
+      return $ Solved (variables solved) ((iterationCount solved) + 1) 
 
--- get the constraint functions out of a list of ConstraintEls
-justConstraints = map getConstraintFromEl
 
--- get values of the variable set
-getValues variables = map (\(Variable _ val _) -> val) variables
-
--- randomizes the variable value of a single variable in a list of variables
-randomizeSingle::Int -> [Variable] -> [IO Variable]
-randomizeSingle variableIndex variables = 
-  map (\x -> if (snd x) == variableIndex then randomizeVariable $ fst x
-                                         else return $ (fst x)) $ zip variables [0..]
-
--- randomize all the variables in a list
-randomize variables = map randomizeVariable variables
-
-printVariables variables = do
-  map (putStrLn . show) variables
-
--- either randomize or let a variable stay, depending on what the constraint
--- check tells us
-update :: Int -> [Variable] -> [ConstraintEl] -> IO [Variable]
-update variableIndex variables constraintSet = do
-  rvariables <- sequence $ randomizeSingle variableIndex variables
-  let values = getValues rvariables
-      constraints = getConstraintsFor variableIndex constraintSet 
-      constraintRes = evalConstraints constraints values
-
-      -- update the variable probability based on the value of constraintRes
-      appliedVars = applyAt (\var -> updateVariableProb var constraintRes) 
-        variableIndex rvariables in
-      return appliedVars
-
--- update each variable in the indices list once
-updateEach' :: [Variable] -> [ConstraintEl] -> [Int] -> IO [Variable]
-updateEach' variables constraintSet (i:indices)
-  | length indices > 0 = do
-    vars <- update i variables constraintSet
-    updateEach' vars constraintSet indices
-  | otherwise  = do
-    return variables
-
--- update each variable in the variable set based on the constraint set
--- value
-updateEach :: [Variable] -> [ConstraintEl] -> IO [Variable]
-updateEach variables constraintSet = 
-  updateEach' variables constraintSet [0 .. (length variables)]
-
--- update the variable set n number of times
-updateEachTimes :: [Variable] -> [ConstraintEl] -> Int -> IO [Variable]
-updateEachTimes variables constraintSet n
-  | n > 0 = do
-    rvars <- updateEach variables constraintSet
-    updateEachTimes rvars constraintSet (n - 1)
-  | otherwise = return variables
+--main = do
+--  rvars <- updateEachTimes vars consts 200
+--  putStrLn $ show consts
+--  putStrLn $ show rvars
+--  putStrLn $ show $ evalConstraint (constraint $ consts !! 0) (getValues rvars)
+--  putStrLn $ show $ getValues rvars
+--  putStrLn $ show $ sort $ getValues rvars
+--    where vars = colorVariables 50
+--          consts = colorConstraints 50
 
 main = do
-  rvars <- updateEachTimes vars consts 200
-  putStrLn $ show consts
-  putStrLn $ show rvars
-  putStrLn $ show $ evalConstraint (constraint $ consts !! 0) (getValues rvars)
-  putStrLn $ show $ getValues rvars 
-    where vars = colorVariables 10
-          consts = colorConstraints 10
+  rsolved <- solve vars consts
+  putStrLn $ show $ getValues $ variables rsolved
+  where vars = finiteVariables 2 50
+        consts = constraints
+
 
